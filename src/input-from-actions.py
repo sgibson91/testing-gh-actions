ded7a5fb91356e4a7763e7c7ec63026d4e29b354
Sarah Gibson <drsarahlgibson@gmail.com>
2022-03-03 20:03:57 +0000

    List cluster names

--- src/input-from-actions.py
+++ src/input-from-actions.py
@@ -2,7 +2,9 @@
 Read in a list of files from the command line and echo the resulting parser, filepath
 collection type, and the filepaths themselves
 """
+import os
 import sys
+import yaml
 import argparse
 import fnmatch
 
@@ -29,6 +31,26 @@ values_files = fnmatch.filter(args.filepaths, "*/*.values.yaml")
 # Filter for support.values.yaml files
 support_files = fnmatch.filter(args.filepaths, "*/support.values.yaml")
 
-if not cluster_files and not values_files and not support_files:
+print(cluster_files)
+print(values_files)
+print(support_files)
+
+if (not cluster_files) and (not values_files) and (not support_files):
     print("Nothing to do!")
     sys.exit()
+
+clusters = []
+# Grab all cluster names from changed values names. This is in case a values file is
+# added/modified but not the associated cluster.yaml file.
+for values_file in values_files:
+    cluster = os.path.basename(os.path.dirname(values_file))
+    if cluster not in clusters:
+        clusters.append(cluster)
+# Now check all added/modified cluster files incase we've missed one
+for cluster_file in cluster_files:
+    with open(cluster_file) as f:
+        cluster = yaml.safe_load(f)
+    if cluster["name"] not in clusters:
+        clusters.append(cluster)
+
+print(clusters)
