33b151883ee73fcacbdf81583edae927cd427dce
Sarah Gibson <drsarahlgibson@gmail.com>
2022-03-04 10:21:06 +0000

    Trying a different approach

--- src/input-from-actions.py
+++ src/input-from-actions.py
@@ -24,33 +24,38 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-# Filter for cluster.yaml files
-cluster_files = fnmatch.filter(args.filepaths, "*/cluster.yaml")
-# Filter for *.values.yaml files
-values_files = fnmatch.filter(args.filepaths, "*/*.values.yaml")
-# Filter for support.values.yaml files
-support_files = fnmatch.filter(args.filepaths, "*/support.values.yaml")
+# Identify unique cluster paths
+filepaths = [os.path.basename(filepath) for filepath in args.filepaths]
+cluster_filepaths = list(set(filepaths))
+print(cluster_filepaths)
 
-print(cluster_files)
-print(values_files)
-print(support_files)
+# # Filter for cluster.yaml files
+# cluster_files = fnmatch.filter(args.filepaths, "*/cluster.yaml")
+# # Filter for *.values.yaml files
+# values_files = fnmatch.filter(args.filepaths, "*/*.values.yaml")
+# # Filter for support.values.yaml files
+# support_files = fnmatch.filter(args.filepaths, "*/support.values.yaml")
 
-if (not cluster_files) and (not values_files) and (not support_files):
-    print("Nothing to do!")
-    sys.exit()
+# print(cluster_files)
+# print(values_files)
+# print(support_files)
 
-clusters = []
-# Grab all cluster names from changed values names. This is in case a values file is
-# added/modified but not the associated cluster.yaml file.
-for values_file in values_files:
-    cluster = os.path.basename(os.path.dirname(values_file))
-    if cluster not in clusters:
-        clusters.append(cluster)
-# Now check all added/modified cluster files incase we've missed one
-for cluster_file in cluster_files:
-    with open(cluster_file) as f:
-        cluster = yaml.safe_load(f)
-    if cluster["name"] not in clusters:
-        clusters.append(cluster["name"])
+# if (not cluster_files) and (not values_files) and (not support_files):
+#     print("Nothing to do!")
+#     sys.exit()
 
-print(clusters)
+# clusters = []
+# # Grab all cluster names from changed values names. This is in case a values file is
+# # added/modified but not the associated cluster.yaml file.
+# for values_file in values_files:
+#     cluster = os.path.basename(os.path.dirname(values_file))
+#     if cluster not in clusters:
+#         clusters.append(cluster)
+# # Now check all added/modified cluster files incase we've missed one
+# for cluster_file in cluster_files:
+#     with open(cluster_file) as f:
+#         cluster = yaml.safe_load(f)
+#     if cluster["name"] not in clusters:
+#         clusters.append(cluster["name"])
+
+# print(clusters)
