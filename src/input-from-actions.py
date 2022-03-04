a4da76238acdc6f2e983195de845678d0edbcd77
Sarah Gibson <drsarahlgibson@gmail.com>
2022-03-04 11:40:07 +0000

    bug fix

--- src/input-from-actions.py
+++ src/input-from-actions.py
@@ -34,8 +34,10 @@ def generate_basic_cluster_info(cluster_name, provider, needs=[]):
     cluster_info = {
         "cluster_name": cluster_name,
         "provider": provider,
-        "needs": ["decision"].extend(needs),
+        "needs": ["decision"],
     }
+
+    cluster_info["needs"].extend(needs)
 
     return cluster_info
 
